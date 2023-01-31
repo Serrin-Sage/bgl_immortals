467e2c9b45048c375f793c826c91c20cca3b417c
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-31 14:11:35 -0500

    started working on the main-page, have general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
-import MainPage from './components/MainPage'
+import Header from './components/main/Header'
+import MainPage from './components/main/MainPage'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<MainPage />} />
