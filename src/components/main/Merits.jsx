cae73eefc489217a2b08a46e2bb0a46f6304daa5
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-02-01 14:41:19 -0500

    added navbar to merit page, added more css

--- src/components/main/Merits.jsx
+++ src/components/main/Merits.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import MeritDisplay from "./MeritDisplay"
 
 const Merits = () => {
     const [allMerits, setAllMerits] = useState([])
     let characterMerits = []
     let designMerits = []
+
     useEffect(() => {
         const fetchMerits = async () => {
             let req = await fetch("http://localhost:3000/merits")
@@ -18,6 +20,13 @@ const Merits = () => {
         fetchMerits()
     },[])
 
+    const scrollToSection = (section) => {
+        const element = document.getElementById(section);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     characterMerits = allMerits.filter((merit) => {
         return merit.category === "character"
     })
@@ -27,10 +36,19 @@ const Merits = () => {
 
     return (
         <div className="merit-page-container">
-            Merit Page
+            <div className="merit-page-navbar">
+                <div className="navbar-tab" id="character-tab" onClick={() => scrollToSection("Character")}>Character</div>
+                <div className="navbar-tab" id="design-tab" onClick={() => scrollToSection("Design")}>Desgin</div>
+                <div className="navbar-tab" id="virtue-tab" onClick={() => scrollToSection("Virtue")}>Virtue</div>
+                <div className="navbar-tab" id="tactical-tab" onClick={() => scrollToSection("Tactical")}>Tactical</div>
+                <div className="navbar-tab" id="dedication-tab" onClick={() => scrollToSection("Dedication")}>Dedication</div>
+                <div className="navbar-tab" id="research-tab" onClick={() => scrollToSection("Research")}>Research</div>
+                <div className="navbar-tab" id="victory-tab" onClick={() => scrollToSection("Victory")}>Victory</div>
+                <div className="navbar-tab" id="experience-tab" onClick={() => scrollToSection("Experience")}>Experience</div>
+            </div>
             <div className="merit-page-content">
-                <MeritDisplay meritArray={characterMerits} />
-                <MeritDisplay meritArray={designMerits} />
+                <MeritDisplay meritArray={characterMerits} meritCategory="Character" id="Character"/>
+                <MeritDisplay meritArray={designMerits} meritCategory="Design" id="Desgin"/>
             </div>
         </div>
     )
